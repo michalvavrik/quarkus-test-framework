45375c5f70fae2ab46569af659a7f3e4146a2bcf
Michal Vavřík <mvavrik@redhat.com>
2023-09-03 19:30:55 +0200

    wip4

--- quarkus-test-core/src/main/java/io/quarkus/test/utils/ProcessBuilderProvider.java
+++ quarkus-test-core/src/main/java/io/quarkus/test/utils/ProcessBuilderProvider.java
@@ -1,5 +1,8 @@
 package io.quarkus.test.utils;
 
+import static java.lang.String.format;
+import static java.lang.String.join;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +16,7 @@ public final class ProcessBuilderProvider {
     public static ProcessBuilder command(List<String> command) {
         List<String> effectiveCommand = new ArrayList<>(command);
         if (OS.WINDOWS.isCurrentOs()) {
-            effectiveCommand = List.of("cmd", "/c", "\"", String.join(" ", effectiveCommand), "\"");
+            effectiveCommand = List.of("cmd", "/c", format("\"%s\"", join(" ", effectiveCommand)));
         }
 
         return new ProcessBuilder(effectiveCommand);
