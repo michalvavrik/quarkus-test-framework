bba65cd380074e7638bc77c433442fd8faf7596e
Michal Vavřík <mvavrik@redhat.com>
2023-09-05 17:40:28 +0200

    wip67

--- quarkus-test-core/src/main/java/io/quarkus/test/utils/ProcessBuilderProvider.java
+++ quarkus-test-core/src/main/java/io/quarkus/test/utils/ProcessBuilderProvider.java
@@ -12,10 +12,6 @@ public final class ProcessBuilderProvider {
 
     public static ProcessBuilder command(List<String> command) {
         List<String> effectiveCommand = new ArrayList<>(command);
-        if (OS.WINDOWS.isCurrentOs()) {
-            // blah
-        }
-
         return new ProcessBuilder(effectiveCommand);
     }
 }
