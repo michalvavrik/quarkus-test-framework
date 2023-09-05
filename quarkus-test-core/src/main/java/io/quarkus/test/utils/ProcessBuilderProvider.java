b2c70af128d77ad6f325a464d8d5c97e3a0238d3
Michal Vavřík <mvavrik@redhat.com>
2023-09-05 18:18:39 +0200

    wip6

--- quarkus-test-core/src/main/java/io/quarkus/test/utils/ProcessBuilderProvider.java
+++ quarkus-test-core/src/main/java/io/quarkus/test/utils/ProcessBuilderProvider.java
@@ -1,7 +1,9 @@
 package io.quarkus.test.utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class ProcessBuilderProvider {
     private ProcessBuilderProvider() {
@@ -9,7 +11,10 @@ public final class ProcessBuilderProvider {
     }
 
     public static ProcessBuilder command(List<String> command) {
-        List<String> effectiveCommand = new ArrayList<>(command);
-        return new ProcessBuilder(effectiveCommand);
+        List<String> effectiveCommand = new ArrayList<>();
+        effectiveCommand.add(command.get(0));
+        effectiveCommand.addAll(command.stream().skip(1).map(smth -> "\"" + smth + "\"").collect(Collectors.toList()));
+        System.out.println("using command " + Arrays.toString(effectiveCommand.toArray()));
+        return new ProcessBuilder();
     }
 }
