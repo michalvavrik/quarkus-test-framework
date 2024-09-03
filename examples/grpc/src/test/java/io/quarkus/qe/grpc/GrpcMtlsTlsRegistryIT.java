1f737ae84eb18d3b4a7fd009dd457b6f92dc98bb
Michal Vavřík <mvavrik@redhat.com>
2024-09-03 10:50:05 +0200

    wip

--- examples/grpc/src/test/java/io/quarkus/qe/grpc/GrpcMtlsTlsRegistryIT.java
+++ examples/grpc/src/test/java/io/quarkus/qe/grpc/GrpcMtlsTlsRegistryIT.java
@@ -17,14 +17,18 @@ public class GrpcMtlsTlsRegistryIT {
     private static final String CN = "Hagrid";
     private static final String NAME = "Albus";
 
-    @QuarkusApplication(grpc = true, ssl = true, certificates = @Certificate(format = PEM, configureHttpServer = true, configureKeystore = true, configureTruststore = true, tlsConfigName = "grpc-tls", clientCertificates = @ClientCertificate(cnAttribute = CN)))
+    @QuarkusApplication(grpc = true, ssl = true)
     static final GrpcService app = (GrpcService) new GrpcService()
             .withProperty("quarkus.grpc.server.use-separate-server", "false")
             .withProperty("quarkus.http.insecure-requests", "disabled")
             .withProperty("quarkus.http.ssl.client-auth", "request")
             .withProperty("quarkus.http.auth.permission.perm-1.policy", "authenticated")
             .withProperty("quarkus.http.auth.permission.perm-1.paths", "*")
-            .withProperty("quarkus.http.auth.permission.perm-1.auth-mechanism", "X509");
+            .withProperty("quarkus.http.auth.permission.perm-1.auth-mechanism", "X509")
+            .withProperty("quarkus.tls.grpc-tls.trust-store.pem.certs", "C:\\cygwin\\tmp\\quarkus-qe-certs3246157749933556819\\quarkus-qe-server-ca.crt")
+            .withProperty("quarkus.tls.grpc-tls.key-store.pem.pem-1.cert", "C:\\cygwin\\tmp\\quarkus\n" +
+                    "-qe-certs3246157749933556819\\quarkus-qe.crt")
+            .withProperty("quarkus.tls.grpc-tls.key-store.pem.pem-1.key", "C:\\cygwin\\tmp\\quarkus-qe-certs3246157749933556819\\quarkus-qe.key");
 
     @Test
     public void testMutualTlsCommunicationWithHelloService() {
